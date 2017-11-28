336eaf2a15cab79018986d471c54d66e51330c42
miriambellver <miriambellver@users.noreply.github.com>
2017-11-28 17:58:19 +0100

    Update eval_cityscapes.sh

--- scripts/eval_cityscapes.sh
+++ scripts/eval_cityscapes.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python eval.py -model_name=eval_cityscapes -dataset=cityscapes -batch_size=5 -maxseqlen=20  --no_run_coco --display -cityscapes_dir=/gpfs/scratch/bsc31/bsc31429/CityScapes/ --log_term
+python eval_cityscapes.py -model_name=cityscapes -dataset=cityscapes -batch_size=5 -maxseqlen=20  --no_run_coco --display -cityscapes_dir=/gpfs/scratch/bsc31/bsc31429/CityScapes/ --log_term
