ebc2bd13067aaab385a9a323ec504477632de18b
miriambellver <miriambellver@users.noreply.github.com>
2017-11-28 17:50:03 +0100

    Update display_leaves.sh

--- scripts/display_leaves.sh
+++ scripts/display_leaves.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python eval.py -model_name=leaves -dataset=leaves  -batch_size=5 -maxseqlen=2 --resize -imsize=400 --display --log_term
+python eval.py -model_name=leaves -dataset=leaves  -batch_size=5 -maxseqlen=20 --resize -imsize=400 --display --log_term
