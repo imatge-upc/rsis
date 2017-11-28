ca4c41c4281f53807a507eeeaa66b6042a8d6c5b
amaiasalvador <amaia.salvador@upc.edu>
2017-11-28 18:50:44 +0100

    scripts updated

--- scripts/train_leaves.sh
+++ scripts/train_leaves.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python train.py -model_name=rsis-leaves -max_epoch=10000 -dataset=leaves -num_classes=2 --augment --resize -maxseqlen=20 -gt_maxseqlen=20 -patience_stop=500  -base_model=resnet101 -class_loss_after=-1 -ngpus=2 -batch_size=20 -patience=30 -stop_loss_after=500 --curriculum_learning -min_steps=5 -stop_weight=0.1 -imsize=400 -leaves_dir=/gpfs/scratch/bsc31/bsc31429/LeavesDataset/A1/ --log_term
+python train.py -model_name=leaves -max_epoch=10000 -dataset=leaves -num_classes=2 --augment --resize -maxseqlen=20 -gt_maxseqlen=20 -patience_stop=500  -base_model=resnet101 -class_loss_after=-1 -ngpus=2 -batch_size=20 -patience=30 -stop_loss_after=500 --curriculum_learning -min_steps=5 -stop_weight=0.1 -imsize=400 --log_term
