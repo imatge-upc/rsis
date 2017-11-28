248327ec4c7f4e44f249c417616a7ff362634404
miriambellver <miriambellver@users.noreply.github.com>
2017-11-28 17:54:45 +0100

    Create eval_cityscapes.sh

---
+++ scripts/eval_cityscapes.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python eval.py -model_name=eval_cityscapes -dataset=cityscapes -batch_size=5 -maxseqlen=20  --no_run_coco --display -cityscapes_dir=/gpfs/scratch/bsc31/bsc31429/CityScapes/ --log_term
